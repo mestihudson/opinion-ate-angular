c4206a96806dc6a42466def48ab476831a5be4f2
Hudson Ferreira Leite <mestihudson@gmail.com>
2025-05-03 13:00:52 -0300

    chore: add karma config file to allow run karma/jasmine unit tests on headless mode

---
+++ opinion-ate/karma.conf.js
@@ -0,0 +1,19 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          "--no-sandbox",
+        ],
+      },
+    },
+  });
+};
