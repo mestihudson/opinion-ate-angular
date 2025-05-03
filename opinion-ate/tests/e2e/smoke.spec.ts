324c1d6f5d7f25f8c7b7f080db47800a710a238e
Hudson Ferreira Leite <mestihudson@gmail.com>
2025-05-03 20:35:13 -0300

    chore: change description of smoke e2e test

--- opinion-ate/tests/e2e/smoke.spec.ts
+++ opinion-ate/tests/e2e/smoke.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test.describe('smoke test', () => {
-  test('has title', async ({ page }) => {
+  test('can view the home page', async ({ page }) => {
     await page.goto('/');
     await expect(page.getByText('Hello, opinion-ate')).toBeVisible();
   });
