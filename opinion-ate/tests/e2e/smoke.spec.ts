6b06aa056ede3bac1a9b7b3e549463f69f825b9a
Hudson Ferreira Leite <mestihudson@gmail.com>
2025-05-03 15:33:05 -0300

    chore: add smoke e2e test to verify comunication between services/containers (e2e => dev)

---
+++ opinion-ate/tests/e2e/smoke.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('smoke test', () => {
+  test('has title', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.getByText("Hello, opinion-ate")).toBeVisible();
+  });
+})
